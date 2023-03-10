cb5e72a6aaa1a402e7d7ea1da7fd347cc9dbe236
Faizan9712 <faizu97123@gmail.com>
2023-03-10 18:10:57 +0530

    hgui

--- index.ts
+++ index.ts
@@ -53,7 +53,7 @@ app.listen(PORT, () => {
 
 //TESTING API
 app.get("/", (req: Request, res: Response) => {
-  res.send("<h1>Hey Faizan</h1>");
+  res.send("<h1>Happy Birthday Ms Rutba...</h1>");
 });
 
 //BINDING ROUTES
